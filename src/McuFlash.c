a49191a64998e970208fd3641e5fb82e6bc5a698
Erich Styger <erich.styger@hslu.ch>
2026-07-30 07:32:53 +0200

    improved K22FX512 flash programming

--- src/McuFlash.c
+++ src/McuFlash.c
@@ -146,7 +146,8 @@ static uint8_t McuFlash_ProgramPage(void *addr, const void *data, size_t dataSiz
   if (McuFlash_Erase(addr, dataSize)!=ERR_OK) {
     return ERR_FAILED;
   }
-#if McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K02FN || McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K22FN
+#if  McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K02FN \
+  || McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K22FN
   /* need to switch to normal RUN mode for flash programming,
    * with Fcore=60MHz Fbus=Fflash=20MHz
    * see https://community.nxp.com/thread/377633
@@ -158,7 +159,7 @@ static uint8_t McuFlash_ProgramPage(void *addr, const void *data, size_t dataSiz
   McuWait_Waitms(1); /* give time to switch clock, otherwise flash programming might fail below */
 #endif
   /* program */
-#if McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K02FN
+#if McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K22FX || McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K02FN
     uint32_t primask = DisableGlobalIRQ(); /* workaround: need to disable interrupts? */
 #endif
   for(;;) { /* breaks, switch back to HSRUN if things fail */
@@ -177,14 +178,17 @@ static uint8_t McuFlash_ProgramPage(void *addr, const void *data, size_t dataSiz
     #endif
     break;
   } /* for */
-#if McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K02FN
+#if McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K22FX || McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K02FN
   EnableGlobalIRQ(primask); /* workaround: need to disable interrupts? */
 #endif
-#if McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K02FN || McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K22FN
+#if   McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K02FN \
+   || McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K22FN
   status = SMC_SetPowerModeHsrun(SMC);
   if (status!=kStatus_Success) {
     res = ERR_FAILED;
   }
+#elif  McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K22FX
+
 #endif
   return res;
 #elif McuLib_CONFIG_CPU_IS_LPC55xx
@@ -401,7 +405,8 @@ uint8_t McuFlash_Erase(void *addr, size_t nofBytes) {
   if (McuFlash_IsErased(addr, nofBytes)) { /* already eased? */
     return ERR_OK; /* yes, nothing to do */
   }
-  #if McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K02FN || McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K22FN
+  #if    McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K02FN \
+      || McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K22FN
   /* need to switch to normal RUN mode for flash programming,
    * with Fcore=60MHz Fbus=Fflash=20MHz
    * see https://community.nxp.com/thread/377633
@@ -429,7 +434,7 @@ uint8_t McuFlash_Erase(void *addr, size_t nofBytes) {
   }
 
   for(;;) { /* breaks, switch back to HSRUN on Kinetis if things fail */
-  #if McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K02FN
+  #if McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K02FN || McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K22FX
     uint32_t primask = DisableGlobalIRQ(); /* workaround: need to disable interrupts? */
   #endif
   #if McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K22FX
@@ -440,12 +445,12 @@ uint8_t McuFlash_Erase(void *addr, size_t nofBytes) {
     if (status!=kStatus_FTFx_Success) {
   #endif
       res = ERR_FAILED;
-    #if McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K02FN
+    #if McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K02FN || McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K22FX
       EnableGlobalIRQ(primask); /* workaround: need to disable interrupts? */
     #endif
       break; /* error, leave for(;;) loop */
     }
-  #if McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K02FN
+  #if McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K02FN || McuLib_CONFIG_CPU_VARIANT==McuLib_CONFIG_CPU_VARIANT_NXP_K22FX
     EnableGlobalIRQ(primask); /* workaround: need to disable interrupts? */
   #endif
     /* Verify sector if it's been erased. */
